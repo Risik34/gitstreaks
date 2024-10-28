774f99874059ae65059f834de28f982cafb7a567
Risik34 <risimn7@gmail.com>
2024-10-28 14:03:01 +0530

    feat: Adds schema validation for habit

--- server/src/routes/habit.ts
+++ server/src/routes/habit.ts
@@ -2,14 +2,22 @@ import { Hono } from 'hono';
 import prisma from '../utils/prisma.js';
 import { authenticateJWT } from '../middleware/authenticateJWT.js';
 import customlogger from '../utils/logger.js';
+import { zValidator } from '@hono/zod-validator';
+import { habitSchema } from '../schema.js';
+
+interface User {
+  id: string;
+  email: string;
+  password: string;
+}
 
 const habit = new Hono().basePath('/habit');
 
-habit.post('/', authenticateJWT, async c => {
-  const user = c.get('user');
+habit.post('/', authenticateJWT, zValidator('json', habitSchema), async c => {
+  const user = c.get('user') as User;
   customlogger(user);
 
-  const data = await c.req.json();
+  const data = c.req.valid('json');
   customlogger(data);
 
   const habit = await prisma.habit.create({
